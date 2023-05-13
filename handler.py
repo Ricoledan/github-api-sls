6606d234638706ef267e8a7f9a8d8965a71cb5a7
Ricardo <ricoledan@gmail.com>
2023-05-13 00:59:47 -0400

    updates

--- handler.py
+++ handler.py
@@ -1,8 +1,8 @@
 import os
 import json
+import time
 from typing import Union, Any
-from github import Github
-from github import RateLimitExceededException
+from github import Github, RateLimitExceededException
 from datetime import datetime, timedelta
 import pandas as pd
 
@@ -30,40 +30,40 @@ def info(event, context):
                                   'Number of releases in the past year': []}
 
     for repo_name in repositories:
-        try:
-            repo = g.get_repo(repo_name)
+        while True:
+            try:
+                repo = g.get_repo(repo_name)
 
-            contributors = repo.get_contributors()
-            contributors_count = contributors.totalCount
+                contributors = repo.get_contributors()
+                contributors_count = contributors.totalCount
 
-            one_year_ago = datetime.now() - timedelta(days=365)
+                one_year_ago = datetime.now() - timedelta(days=365)
 
-            releases = repo.get_releases()
-            yearly_releases = 0
+                releases = repo.get_releases()
+                yearly_releases = 0
 
-            for release in releases:
-                release_date = release.published_at
+                for release in releases:
+                    release_date = release.published_at
 
-                if release_date > one_year_ago:
-                    yearly_releases += 1
-                elif release_date < one_year_ago:
-                    break
+                    if release_date > one_year_ago:
+                        yearly_releases += 1
+                    elif release_date < one_year_ago:
+                        break
 
-            data['Repository'].append(repo_name)
-            data['Number of contributors'].append(contributors_count)
-            data['Number of releases in the past year'].append(yearly_releases)
+                data['Repository'].append(repo_name)
+                data['Number of contributors'].append(contributors_count)
+                data['Number of releases in the past year'].append(yearly_releases)
 
-        except RateLimitExceededException:
-            rate_limit_reset_time = g.rate_limiting_resettime
-            current_time = datetime.now().timestamp()
-            sleep_time = max(rate_limit_reset_time - current_time, 0)
+                break
 
-            return {
-                'statusCode': 429,
-                'body': json.dumps({
-                    'message': f"Rate limit exceeded. Please wait for {sleep_time} seconds for the rate limit to reset."
-                })
-            }
+            except RateLimitExceededException:
+                rate_limit_reset_time = g.rate_limiting_resettime
+                current_time = datetime.now().timestamp()
+                sleep_time = max(rate_limit_reset_time - current_time, 0)
+
+                for i in range(int(sleep_time), 0, -1):
+                    print(f"Rate limit exceeded. Waiting for {i} seconds.")
+                    time.sleep(1)
 
     df = pd.DataFrame(data, columns=['Repository', 'Number of contributors', 'Number of releases in the past year'])
 
